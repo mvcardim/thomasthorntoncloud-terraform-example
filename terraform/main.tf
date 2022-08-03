caca098a9f3234fad742c55a69b83163b99e2455
marco cardim <mvcardim@hotmail.com>
2022-08-03 11:09:49 -0300

    alterado fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,27 +7,27 @@ terraform {
   }
 }
 provider "azurerm" {
-    features {}
+  features {}
 }
 resource "azurerm_resource_group" "rg" {
 
-    name = "rg-terraform-import"
-    location = "brazilsouth"
-    tags = {
-        "ambiente" = "treinamento"
-    }
+  name     = "rg-terraform-import"
+  location = "brazilsouth"
+  tags = {
+    "ambiente" = "treinamento"
+  }
 }
 resource "azurerm_virtual_network" "vnet" {
-   name = "vnet-import"
-   resource_group_name = "rg-terraform-import"
-   location = "brazilsouth"
-   address_space = [ "10.0.0.0/16" , "192.168.0.0/16"]
-   tags = {
-     "ambiente" = "teste"
-   }
+  name                = "vnet-import"
+  resource_group_name = "rg-terraform-import"
+  location            = "brazilsouth"
+  address_space       = ["10.0.0.0/16", "192.168.0.0/16"]
+  tags = {
+    "ambiente" = "teste"
+  }
 }
-resource "azurerm_network_security_group" "nsg"{
-    name = "nsg-import"
-    location = "brazilsouth"
-    resource_group_name = "rg-terraform-import"
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-import"
+  location            = "brazilsouth"
+  resource_group_name = "rg-terraform-import"
 }

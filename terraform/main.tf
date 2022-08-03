5b48b483dea96948620d74cc4dfe14c8be2a8e94
marco cardim <mvcardim@hotmail.com>
2022-08-03 09:45:14 -0300

    Removido backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,27 @@
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "tamopstfstates"
+#    storage_account_name = "tfstatedevops"
+#    container_name       = "terraformgithubexample"
+#    key                  = "terraformgithubexample.tfstate"
+# }
+#}
+
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tfstatedevops"
-    container_name       = "terraformgithubexample"
-    key                  = "terraformgithubexample.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
   }
+
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  source  = "hashicorp/azurerm"
-  version = "~> 3.0.2"
+  features {}
 }
+ 
 
 data "azurerm_client_config" "current" {}
 

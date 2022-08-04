f8f33f64aca677f77bf48fed55857aa75e85f0b5
marco cardim <mvcardim@hotmail.com>
2022-08-04 10:00:43 -0300

    inclusao de tags e variaveis

---
+++ terraform/variaveis.tf
@@ -0,0 +1,18 @@
+variable "location" {
+  type = string
+  description = "Localizacao dos Rercursos do Azure. Ex: brazilsouth"
+  default = "eastus"
+}
+ variable "namerg" {
+  type = string
+  description = "Nome do Resource Group"
+  default = "rg-variaveis"
+}
+variable "tags" {
+  type = map
+  description = "Tags nos Recursos e Servicos do azure"
+  default = {
+      ambiente = "desenvolvimento"
+      responsavel = "Marco Vinicio"
+  }
+}
